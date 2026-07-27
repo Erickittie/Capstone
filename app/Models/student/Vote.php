c517b8740e80894aec9cf43216344be0023dd369
mccainzkie912@gmail.com <mccainzkie912@gmail.com>
2026-07-28 02:52:42 +0800

    completed the vote model

--- app/Models/student/Vote.php
+++ app/Models/student/Vote.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vote extends Model
 {
-    //
+    // Fields that are allowed to be saved to the database
+    protected $fillable = [
+        'voter_id',    
+        'candidate_id', 
+        'group_id',   
+    ];
+
+    // Get the student who cast this vote
+    public function voter()
+    {
+        return $this->belongsTo(User::class, 'voter_id');
+    }
+
+    // Get the student who received this vote
+    public function candidate()
+    {
+        return $this->belongsTo(User::class, 'candidate_id');
+    }
 }
