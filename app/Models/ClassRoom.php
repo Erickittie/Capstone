c6daab10943c94d781952318f61a59de0b174db8
Erickittie <dumalaganjererick@gmail.com>
2026-09-08 20:59:13 +0800

    fix the classroom model

--- app/Models/ClassRoom.php
+++ app/Models/ClassRoom.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ClassRoom extends Model
 {
@@ -16,8 +19,24 @@ class ClassRoom extends Model
         'Instructor_Id'
     ];
 
-    public function instructor() {
+    public function instructor(): BelongsTo {
         
         return $this->belongsTo(User::class, 'Instructor_Id', 'id');
+    }
+
+    public function students(): BelongsToMany {
+        return $this -> belongsToMany (
+            User::class,
+            'class_student',
+            'class_room_id',
+            'student_id'
+        ) -> withTimestamps();
+    }
+
+    public function groups(): HasMany {
+        return $this -> hasMany(
+            Group::class,
+            'class_room_id'
+        );
     }
 }
