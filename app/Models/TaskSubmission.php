c322166f90c328fc1cc992a86cd75d9d1ee9bc56
Erickittie <dumalaganjererick@gmail.com>
2026-09-08 20:35:48 +0800

    add a task submission model

---
+++ app/Models/TaskSubmission.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Ulluminate\Database\Eloquent\Relations\BelongsTo;
+
+class TaskSubmission extends Model
+{
+    //
+    protected $fillable  = [
+        'task_id',
+        'student_id',
+        'submission_text',
+        'file_path',
+        'status',
+        'submitted_at',
+        'approved_at',
+        'approved_by',
+        'feedback',
+    ];
+
+    protected $casts = [
+        'submitted_at' => 'datetime',
+        'approved_at' => 'datetime',
+    ];
+
+    public function task(): BelongsTo
+    {
+        return $this->belongsTo(
+            Task::class,
+            'task_id'
+        );
+    }
+
+     public function student(): BelongsTo
+    {
+        return $this->belongsTo(
+            User::class,
+            'student_id'
+        );
+    }
+
+    public function approver(): BelongsTo
+    {
+        return $this->belongsTo(
+            User::class,
+            'approved_by'
+        );
+    }
+}
