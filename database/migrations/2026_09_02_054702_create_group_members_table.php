873d1c1f6de2a2413aafab2bedb9e19d23584a92
Erickittie <dumalaganjererick@gmail.com>
2026-09-08 20:31:28 +0800

    add a group members table

---
+++ database/migrations/2026_09_02_054702_create_group_members_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('group_members', function (Blueprint $table) {
+            $table->id();
+            $table -> foreignId('group_id')
+            -> constrained('groups')
+            -> cascadeOnDelete();
+
+            $table -> foreignId('student_id')
+            -> contrained('users')
+            -> cascadeOnDelete();
+
+            $table -> boolean('is_leader')
+            -> default(false);
+
+            $table->timestamps();
+
+            $table -> unique([
+                'group_id',
+                'student_id'
+            ]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('group_members');
+    }
+};
