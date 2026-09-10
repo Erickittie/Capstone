4f93fc38f121da24d63fc5bf3432c6b8b9b9c37b
Erickittie <dumalaganjererick@gmail.com>
2026-09-10 10:19:09 +0800

    fix the group model

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -2,34 +2,55 @@
 
 namespace App\Models;
 
+use App\Models\Student\GroupLeaderVote;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Group extends Model
 {
-    //
     protected $fillable = [
         'class_room_id',
         'name',
         'group_number',
     ];
 
-    public function classRoom(): BelongsTo {
-        return $this -> belongsTo(
+    public function classRoom(): BelongsTo
+    {
+        return $this->belongsTo(
             ClassRoom::class,
             'class_room_id'
         );
     }
 
-    public function students() : BelongsToMany {
-        return $this -> belongsToMany(
+    public function students(): BelongsToMany
+    {
+        return $this->belongsToMany(
             User::class,
             'group_members',
             'group_id',
             'student_id'
         )
-        -> withPivot('is_leader')
-        -> withTimestamps();
+        ->withPivot('is_leader')
+        ->withTimestamps();
+    }
+
+    public function leaderVotes(): HasMany
+    {
+        return $this->hasMany(
+            GroupLeaderVote::class,
+            'group_id'
+        );
+    }
+
+    public function projects(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Project::class,
+            'project_groups',
+            'group_id',
+            'project_id'
+        )->withTimestamps();
     }
 }
