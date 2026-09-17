ae71f781564c807671bbda975b088c32b7962de6
Erickittie <dumalaganjererick@gmail.com>
2026-09-17 22:53:09 +0800

    rename the section to offer code in class rooms table

---
+++ database/migrations/2026_09_16_022543_rename_section_to_offer_code_in_class_rooms_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('class_rooms', function (Blueprint $table) {
+            $table->renameColumn('section', 'offer_code');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('class_rooms', function (Blueprint $table) {
+            $table->renameColumn('offer_code', 'section');
+        });
+    }
+};
